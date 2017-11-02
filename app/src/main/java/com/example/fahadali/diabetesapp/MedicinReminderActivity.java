3b72b9890a57d72a92bd48ddd85b063d36a5ec86
Emil Frederik Rune Jørgensen <Emil-Jorgensen@live.dk>
2017-11-02 14:36:36 +0100

    Oprettelse af fragmenter (.java /.XML) til påmindelser for medicin. Tilføjet strings og colors i values

---
+++ app/src/main/java/com/example/fahadali/diabetesapp/MedicinReminderActivity.java
@@ -0,0 +1,10 @@
+package com.example.fahadali.diabetesapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by emiljorgensen on 02/11/2017.
+ */
+
+public class MedicinReminderActivity extends Fragment {
+}
