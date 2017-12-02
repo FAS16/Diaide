6b5580e2cb0561bae6d410ee2d3f849b91d157b8
Emil Frederik Rune Jørgensen <Emil-Jorgensen@live.dk>
2017-12-02 11:33:13 +0100

    Usertest fix

--- app/src/test/java/com/example/fahadali/diabetesapp/UserTest.java
+++ app/src/test/java/com/example/fahadali/diabetesapp/UserTest.java
@@ -9,36 +9,19 @@ import static org.junit.Assert.*;
 
 public class UserTest {
     @Test
-    public void createUserTest(){
+    public void createUserTest() {
 
         User user = new User("1", "Martin", "martindahljen@gmail.com", "qwerty12");
-        assertEquals("Martin", user.getName());
+        assertEquals("Martin", user.getFirstName());
         assertEquals("martindahljen@gmail.com", user.getMail());
-        assertEquals("qwerty12", user.getPassword());
         assertEquals("1", user.getId());
 
-        user.setName("Aleksander");
+        user.setFirstName("Aleksander");
         user.setMail("awie@gmail.com");
-        user.setPassword("qwerty21");
 
-        assertEquals("Aleksander", user.getName());
+        assertEquals("Aleksander", user.getFirstName());
         assertEquals("awie@gmail.com", user.getMail());
-        assertEquals("qwerty21", user.getPassword());
-
-        }
-    @Test
-    public void createUserDBTest(){
-
-        SignUp_activity testSignup = new SignUp_activity();
-
-        testSignup.createUserAccount("test@email.com", "test123");
-
-
-        FirebaseUser userTest = testSignup.getCurrentUser();
-
-        assertEquals("test@email.com", userTest.getEmail());
-
-        }
-
 
     }
+}
+
