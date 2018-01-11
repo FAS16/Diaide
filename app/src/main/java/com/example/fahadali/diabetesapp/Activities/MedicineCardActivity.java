616f2b9aa57a1a3ef4a057432e9bc42f5c958190
Aleksander <aleksander.wm@gmail.com>
2018-01-11 14:45:34 +0100

    Created medicine card branch

--- app/src/main/java/com/example/fahadali/diabetesapp/Activities/MedicineCardActivity.java
+++ app/src/main/java/com/example/fahadali/diabetesapp/Activities/MedicineCardActivity.java
@@ -3,6 +3,8 @@ package com.example.fahadali.diabetesapp.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.fahadali.diabetesapp.R;
+
 public class MedicineCardActivity extends AppCompatActivity {
 
     @Override
